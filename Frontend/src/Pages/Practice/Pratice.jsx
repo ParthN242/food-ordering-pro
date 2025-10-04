6bad7b443d35c9d97d149e4a9a30e88ccbcea751
ParthN242 <parthnandani3236@gmail.com>
2025-10-04 22:44:35 +0530

    feat/add-one-more-description

--- Frontend/src/Pages/Practice/Pratice.jsx
+++ Frontend/src/Pages/Practice/Pratice.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-
 const Practice = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold">Practice</h1>
       <p>local & remote branch test</p>
+      <p>other commit</p>
     </div>
   );
 };
