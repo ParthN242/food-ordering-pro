ef2b6f5be63dfa324f8d924deb029d3ec0265ac7
ParthN242 <parthnandani3236@gmail.com>
2025-10-04 20:28:43 +0530

    Fix: text of practice route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -50,7 +50,10 @@ function App() {
             <Route path="/orders" element={<Orders />} />
             <Route path="/orders/:id" element={<OrderItems />} />
             <Route path="/practice" element={<Practice />} />
-            <Route path="/practice-route" element={<>Practice Route</>} />
+            <Route
+              path="/practice-route"
+              element={<>----Change Practice Text----</>}
+            />
           </Routes>
           <Footer />
         </Router>
