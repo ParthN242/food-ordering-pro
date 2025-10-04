1f932957ba47a2aeb2cf035554f3e93f5df4997a
ParthN242 <parthnandani3236@gmail.com>
2025-10-04 22:28:40 +0530

    Revert "Merge pull request #7 from ParthN242/feat/add-desciption-in-practice-page"
    This reverts commit 2f2c6b29bb7396fda08a297ed4fc49116c867500, reversing
    changes made to 87446c412302a483c6cf04e9dbfa16d64f7b702c.

--- Frontend/src/Pages/Practice/Pratice.jsx
+++ Frontend/src/Pages/Practice/Pratice.jsx
@@ -1,8 +1,9 @@
+import React from "react";
+
 const Practice = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold">Practice</h1>
-      <p>This is the practice page.</p>
     </div>
   );
 };
